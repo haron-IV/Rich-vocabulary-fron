bfb3ea3765de294584028e0af5d24b45bacd5554
bart <bartek.bober.koder@gmail.com>
2021-05-02 21:50:14 +0200

    add background

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
+import { CssBaseline, styled } from '@material-ui/core'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { routes } from './app'
+import BackgroundImg from './assets/img/Background.jpg'
+
+export const AppStyled = styled('div')({
+  backgroundImage: `url(${BackgroundImg})`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  width: '100vw',
+  height: '100vh',
+})
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        {routes.map(route => (
-          <Route path={route.path}>{route.component}</Route>
-        ))}
-      </Switch>
-    </BrowserRouter>
+    <AppStyled>
+      <CssBaseline />
+      <BrowserRouter>
+        <Switch>
+          {routes.map(route => (
+            <Route path={route.path}>{route.component}</Route>
+          ))}
+        </Switch>
+      </BrowserRouter>
+    </AppStyled>
   )
 }
 
