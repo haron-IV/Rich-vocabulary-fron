732114226ec5fd615b69ca63ab29e49d57cacaab
bart <bartek.bober.koder@gmail.com>
2021-07-24 16:26:26 +0200

    remove something

--- src/shared/services/dictionary/dictionary.ts
+++ src/shared/services/dictionary/dictionary.ts
@@ -1,13 +1,5 @@
-import { AxiosResponse } from 'axios'
 import { API } from '../config'
 import { WordAPI } from './dictionary.types'
-
-// export const initDatabase = async (
-//   request: InitDatabaseRequest
-// ): Promise<AxiosResponse> => API.post('/database/create', request)
-
-// export const checkIfDatabaseExist = async (): Promise<AxiosResponse> =>
-//   API.get('/database/check')
 
 export const getDictionary = async () => {
   const { data } = await API.get<WordAPI[]>('word/all')
